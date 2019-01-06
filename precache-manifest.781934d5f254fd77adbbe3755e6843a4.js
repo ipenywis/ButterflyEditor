fcfcaf66320d7a9f99d8874f9d25c4f8a62d290c
Islem Penywis <ipenywis@gmail.com>
2019-01-06 22:40:55 +0100

    Updates

---
+++ precache-manifest.781934d5f254fd77adbbe3755e6843a4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ButterflyEditor/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "31e4862f09de3d573323d1226361f7e3",
+    "url": "/ButterflyEditor/static/media/islempenywis_picture.31e4862f.jpg"
+  },
+  {
+    "revision": "08b6b608467509e57b0ef7dd930b4122",
+    "url": "/ButterflyEditor/static/media/butterfly-editor-v1.0.0.08b6b608.md"
+  },
+  {
+    "revision": "442c81c06c290c69fdca",
+    "url": "/ButterflyEditor/static/js/runtime~main.442c81c0.js"
+  },
+  {
+    "revision": "f23acf3cf89a33d6e1ec",
+    "url": "/ButterflyEditor/static/js/main.f23acf3c.chunk.js"
+  },
+  {
+    "revision": "574e2a3cfc10902610a6",
+    "url": "/ButterflyEditor/static/js/1.574e2a3c.chunk.js"
+  },
+  {
+    "revision": "f23acf3cf89a33d6e1ec",
+    "url": "/ButterflyEditor/static/css/main.827d0e4b.chunk.css"
+  },
+  {
+    "revision": "574e2a3cfc10902610a6",
+    "url": "/ButterflyEditor/static/css/1.69947bf9.chunk.css"
+  },
+  {
+    "revision": "ef218dbdb6d45686920487e3860250c3",
+    "url": "/ButterflyEditor/index.html"
+  }
+];
